aa463465acb7de97f87b43ee8ed433e301c6b5fc
Andrej Prus <andrej.prus@kodinta.lt>
2025-12-19 16:58:55 +0200

    Enhance auth session and input error alerts with icons and roles

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,10 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'alert alert-success text-sm py-2']) }}>
-        <span>{{ $status }}</span>
+    <div {{ $attributes->merge(['class' => 'alert alert-success text-sm py-2']) }} role="status">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 shrink-0" viewBox="0 0 24 24" fill="currentColor">
+            <path fill-rule="evenodd" d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2Zm4.03 7.47a.75.75 0 0 1 0 1.06l-4.95 4.95a.75.75 0 0 1-1.06 0l-2.05-2.05a.75.75 0 1 1 1.06-1.06l1.52 1.52 4.42-4.42a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+        </svg>
+        <span class="min-w-0">{{ $status }}</span>
     </div>
 @endif
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,16 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-error space-y-1']) }}>
-        @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
-        @endforeach
-    </ul>
+    <div {{ $attributes->merge(['class' => 'alert alert-error py-2']) }} role="alert">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 shrink-0" viewBox="0 0 24 24" fill="currentColor">
+            <path fill-rule="evenodd" d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2Zm.75 6a.75.75 0 0 0-1.5 0v5a.75.75 0 0 0 1.5 0V8ZM12 17a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z" clip-rule="evenodd" />
+        </svg>
+        <div class="min-w-0">
+            <ul class="text-sm leading-snug space-y-1">
+                @foreach ((array) $messages as $message)
+                    <li>{{ $message }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @endif
