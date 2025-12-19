840a065d4cbc44d38b1ed3e74c72a9c4adf02a2c
Andrej Prus <andrej.prus@kodinta.lt>
2025-12-19 11:40:02 +0200

    Refactor welcome page to app layout with timeline/profile CTAs and updat

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,26 +1,11 @@
-<nav class="-mx-3 flex flex-1 justify-end">
+<nav class="-mx-2 flex flex-1 justify-end gap-2">
     @auth
-        <a
-            href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-        >
-            Dashboard
-        </a>
+        <a class="btn btn-ghost btn-sm" href="{{ url('/dashboard') }}" wire:navigate>Dashboard</a>
     @else
-        <a
-            href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-        >
-            Log in
-        </a>
+        <a class="btn btn-ghost btn-sm" href="{{ route('login') }}" wire:navigate>Log in</a>
 
         @if (Route::has('register'))
-            <a
-                href="{{ route('register') }}"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-                Register
-            </a>
+            <a class="btn btn-outline btn-sm" href="{{ route('register') }}" wire:navigate>Register</a>
         @endif
     @endauth
 </nav>
