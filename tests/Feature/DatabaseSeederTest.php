f6a0aabda487f8a88a2c35c6a9e47d4b06b978ae
Andrej Prus <andrej.prus@kodinta.lt>
2025-12-20 14:09:54 +0200

    Add comprehensive DatabaseSeeder with seeding config and feature tests

---
+++ tests/Feature/DatabaseSeederTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class DatabaseSeederTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_database_seeder_creates_expected_records(): void
+    {
+        config([
+            'seeding.model_count' => 3,
+            'seeding.relation_count' => 3,
+        ]);
+
+        Artisan::call('migrate:fresh', ['--seed' => true]);
+
+        $this->assertDatabaseCount('users', 3);
+        $this->assertDatabaseCount('posts', 3);
+        $this->assertDatabaseCount('hashtags', 3);
+        $this->assertDatabaseCount('user_lists', 3);
+        $this->assertDatabaseCount('conversations', 3);
+        $this->assertDatabaseCount('messages', 3);
+        $this->assertDatabaseCount('spaces', 3);
+        $this->assertDatabaseCount('moments', 3);
+
+        $this->assertDatabaseCount('follows', 3);
+        $this->assertDatabaseCount('blocks', 3);
+        $this->assertDatabaseCount('mutes', 3);
+        $this->assertDatabaseCount('likes', 3);
+        $this->assertDatabaseCount('bookmarks', 3);
+        $this->assertDatabaseCount('mentions', 3);
+        $this->assertDatabaseCount('hashtag_post', 3);
+        $this->assertDatabaseCount('post_images', 3);
+        $this->assertDatabaseCount('post_polls', 3);
+        $this->assertDatabaseCount('post_poll_options', 6);
+        $this->assertDatabaseCount('post_poll_votes', 3);
+        $this->assertDatabaseCount('post_link_previews', 3);
+        $this->assertDatabaseCount('conversation_participants', 3);
+        $this->assertDatabaseCount('message_attachments', 3);
+        $this->assertDatabaseCount('message_reactions', 3);
+        $this->assertDatabaseCount('user_list_user', 3);
+        $this->assertDatabaseCount('user_list_subscriptions', 3);
+        $this->assertDatabaseCount('muted_terms', 3);
+        $this->assertDatabaseCount('space_participants', 3);
+        $this->assertDatabaseCount('space_speaker_requests', 3);
+        $this->assertDatabaseCount('space_reactions', 3);
+        $this->assertDatabaseCount('moment_items', 3);
+        $this->assertDatabaseCount('reports', 18);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,12 @@
 
 namespace Tests;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected bool $seed = true;
+
+    protected string $seeder = DatabaseSeeder::class;
 }
